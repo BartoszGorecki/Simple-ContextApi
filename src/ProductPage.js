08bce837294ae14f4554672ba062c1b39dfd981d
Bartosz G <uninstallgame4444@gmail.com>
2019-03-25 09:52:32 +0100

    xx

---
+++ src/ProductPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class ProductPage extends Component {
+    render() {
+        return (
+            <div>ProductPage</div>
+        )
+    }
+}
+export default ProductPage
